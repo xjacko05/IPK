6764a59124e26098d99ea86e08f7602b24c083ea
Martin Jacko <62172219+xjacko05@users.noreply.github.com>
2021-03-28 12:30:08 +0200

    final asi

--- Projekt1/fileget.c
+++ Projekt1/fileget.c
@@ -1,3 +1,11 @@
+/**
+ * Project: IPK 2020 - 1. project
+ *
+ * @brief fileget - file download client
+ *
+ * @author xjacko05 Martin Jacko
+*/
+
 #include <stdio.h> 
 #include <netdb.h> 
 #include <arpa/inet.h>
@@ -11,24 +19,21 @@
 #include <stdbool.h>
 
 
-
 int download(char* fl_ip, int fl_port, char* serv_file, char* serv_name){
-//printf("Hello\n");
-fprintf(stdout, "\nAttempting download:\nfilename:\t%s\nfrom server:\t%s\nwith ip:port:\t%s:%i\n", serv_file, serv_name, fl_ip, fl_port);
-//printf("Hello\n");
+
+    fprintf(stdout, "\nAttempting download:\nfilename:\t%s\nfrom server:\t%s\nwith ip:port:\t%s:%i\n", serv_file, serv_name, fl_ip, fl_port);
+    
+    //socket creation, IP adress fill and server connection
     int fl_sockfd;
     struct sockaddr_in fl_servaddr;
   
-    // socket create and varification 
     fl_sockfd = socket(AF_INET, SOCK_STREAM, 0);
     bzero(&fl_servaddr, sizeof(fl_servaddr));
   
-    // assign IP, PORT 
     fl_servaddr.sin_family = AF_INET; 
     fl_servaddr.sin_addr.s_addr = inet_addr(fl_ip); 
     fl_servaddr.sin_port = htons(fl_port);
   
-    // connect the client socket to server socket 
     if (connect(fl_sockfd, (struct sockaddr *)&fl_servaddr, sizeof(fl_servaddr)) != 0){ 
         fprintf(stderr,"Connection to the file server failed...\n"); 
         exit(0);
@@ -37,21 +42,18 @@ fprintf(stdout, "\nAttempting download:\nfilename:\t%s\nfrom server:\t%s\nwith i
         fprintf(stdout, "\nconnected to the file server..\n"); 
     }
 
-
+    //sending message to server
     char* fl_msg = calloc(46+strlen(serv_name)+strlen(serv_file), sizeof(char));
     sprintf(fl_msg, "GET %s FSP/1.0\r\nHostname: %s\r\nAgent: xjacko05\r\n\r\n", serv_file ,serv_name);
-    //for (int i = 0; i < (int)strlen(fl_msg); i++){
-    //    printf("%i\t%c\n", i, fl_msg[i]);
-    //}
     send(fl_sockfd, fl_msg, strlen(fl_msg), 0);
     sleep(1);
 
+    //reply open as file descriptor
     FILE *sockfile = (FILE *) fdopen(fl_sockfd, "r");
-    int length = 0;
+    long length = 0;
 
     char header[200];
     fgets(header, 200, sockfile);
-    //printf("\n%s\n\n", header);
     if (strstr(header, "Not Found") != NULL){
         fprintf(stderr, "\nFile not found\n");
         exit(1);
@@ -61,12 +63,6 @@ fprintf(stdout, "\nAttempting download:\nfilename:\t%s\nfrom server:\t%s\nwith i
     }
 
     char g = fgetc(sockfile);
-/*
-    while (g != '\n'){
-        //printf("%i\t%c\n", g, g);
-        g = fgetc(sockfile);
-    }
-*/
     while (!isdigit(g)){
         g = fgetc(sockfile);
     }
@@ -74,13 +70,12 @@ fprintf(stdout, "\nAttempting download:\nfilename:\t%s\nfrom server:\t%s\nwith i
         length = (length*10) + (g-48);
         g = fgetc(sockfile);
     }
-    //printf("LENGTH=%i\n", length);
     g = fgetc(sockfile);
     g = fgetc(sockfile);
     g = fgetc(sockfile);
     g = fgetc(sockfile);
 
-    for (int i = 0; i < (int)strlen(serv_file); i++){
+    for (long i = 0; i < (long)strlen(serv_file); i++){
         if (serv_file[i] == '/'){
             serv_file[i] = '.';
         }
@@ -102,11 +97,12 @@ fprintf(stdout, "\nAttempting download:\nfilename:\t%s\nfrom server:\t%s\nwith i
 
 int main(int argc, char const *argv[]){
 
-    //arg parse
+    //arg parse and check
     if (argc != 5){
         fprintf(stderr, "Wrong input\n");
-        exit(1);
+        return 1;
     }
+
     const char* ip_port;
     const char* serv_path;
     
@@ -118,8 +114,9 @@ int main(int argc, char const *argv[]){
         serv_path = argv[2];
     }else{
         fprintf(stderr, "Wrong input\n");
-        exit(1);
+        return 1;
     }
+
     char* tr_ip = NULL;
     int tr_port = 0;
     char* serv_name = NULL;
@@ -128,12 +125,12 @@ int main(int argc, char const *argv[]){
     for (int i = 0; i < (int)strlen(ip_port); ++i){
         if (isdigit(ip_port[i]) == 0 && ip_port[i] != '.' && ip_port[i] != ':'){
             fprintf(stderr, "NAMESERVER containing illegal characters\n");
-            exit(1);
+            return 1;
         }
     }
     if (serv_path[0] != 'f' || serv_path[1] != 's' || serv_path[2] != 'p' || serv_path[3] != ':' || serv_path[4] != '/' || serv_path[5] != '/'){
         fprintf(stderr, "SURL containing illegal PROTOCOL\n");
-        exit(1);
+        return 1;
     }
     
     for (int i = 0; i < (int)strlen(ip_port); i++){
@@ -145,9 +142,6 @@ int main(int argc, char const *argv[]){
             break;
         }
     }
-    //printf("%s-%i\n", tr_ip, tr_port);
-    //printf("%li\n", strlen(tr_ip));
-    
     
     for (int i = 6; i < (int)strlen(serv_path); i++){
         if (serv_path[i] == '/'){
@@ -163,52 +157,31 @@ int main(int argc, char const *argv[]){
         }else if (serv_path[i] != '.' && serv_path[i] != '-' && serv_path[i] != '_' && isalpha(serv_path[i]) == 0 && isdigit(serv_path[i]) == 0){
             fprintf(stderr, "SURL containing illegal SERV_NAME\n");
             free(tr_ip);
-            exit(1);
+            return 1;
         }
     }
 
     if (tr_ip == NULL || serv_file == NULL || serv_name == NULL){
+        fprintf(stderr, "Incorrect parameters\n");
         return 1;
     }
-    /*for (int i = (int)strlen(serv_path) - 1; i > 6 ; i--){
-        if (serv_path[i] == '/'){
-            serv_file = malloc(((int)strlen(serv_path) - i) * sizeof(char));
-            memcpy(serv_file, &serv_path[i+1], ((int)strlen(serv_path) - i - 1));
-            serv_file[(int)strlen(serv_path) - i] = '\0';
-            //printf("O%sO\n",serv_file);
-            break;
-        }
-    }*/
-
-    //printf("%s-%s\n", serv_name, serv_file);
-    //printf("%li-%li-%li\n", strlen(serv_name), strlen(serv_file), strlen(serv_path));
     
-    
-    
+    //connection to translate server
     int sockfd; 
     struct sockaddr_in servaddr; 
   
-    // socket create and varification 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
     bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
+
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = inet_addr(tr_ip); 
     servaddr.sin_port = htons(tr_port); 
-  
-    // connect the client socket to server socket 
+
     connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
     char* tr_msg = calloc(9+strlen(serv_name), sizeof(char));
     strcpy(tr_msg, "WHEREIS ");
     strcat(tr_msg, serv_name);
-    //for (int i = 0; i < (int)strlen(msg); i++){
-    //    printf("%c-%i\n", msg[i], i);
-    //}
-    
-
-    
 
     char* tr_reply = calloc(2000, sizeof(char));
 
@@ -221,11 +194,11 @@ int main(int argc, char const *argv[]){
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
 
     recv(sockfd, tr_reply, 2000 , 0);
-
+    close(sockfd);
 
     if (tr_reply[0] == 'E' && tr_reply[1] == 'R' && tr_reply[2] == 'R'){
         fprintf(stderr, "File server not found\n");
-        exit(0);
+        return 1;
     }
     bool noreply = true;
     for (int i = 0; i < 2000; i++){
@@ -235,11 +208,8 @@ int main(int argc, char const *argv[]){
     }
     if (noreply){
         fprintf(stderr, "No reply from translate server recieved\n");
-        exit(1);
+        return 1;
     }
-
-
-
 
     char* fl_ip_port = &tr_reply[3];
     char* fl_ip;
@@ -253,11 +223,9 @@ int main(int argc, char const *argv[]){
             fl_port = atoi(&fl_ip_port[i+1]);
         }
     }
-    //printf("%s-%i\n", fl_ip, fl_port);
-    //printf("%li\n", strlen(fl_ip));
 
+    //file download
     if (strcmp(serv_file, "*") == 0){
-    
         int l = download(fl_ip, fl_port, "index", serv_name);
         FILE* index = fopen("index", "r");
         char* line = calloc((l+1), sizeof(char));
@@ -266,17 +234,15 @@ int main(int argc, char const *argv[]){
                 if (line[i] == 13 || line[i] == 10){
                     line[i] = 0;
                 }
-                //printf("%i\t%c\n", line[i], line[i]);
             }
             download(fl_ip, fl_port, line, serv_name);
             memset(line, 0, l);
         }
+        fclose(index);
         free(line);
     }else{
         download(fl_ip, fl_port, serv_file, serv_name);
     }
-
-
 
     free(fl_ip);
     free(tr_ip);
@@ -284,19 +250,5 @@ int main(int argc, char const *argv[]){
     free(tr_reply);
     free(serv_file);
     free(serv_name);
-
-    
-    //for (int i = 0; i < 3; i++){
-    //    printf("%i %c\n", i, fgetc(sockfile));
-    //}
-    //fgets(reply1, 7, sockfile);
-    //printf("%s\n%li\n", reply1, strlen(reply1));
-    
-    //recv(sockfd, reply2, 5 , 0);
-    //fgets(reply2, 10, sockfile);
-    //printf("%s\n", reply2);
-  
-    // close the socket 
-    close(sockfd);
     return 0;
 }
